690a8ee8cd871349eb817bda7474e68e46a0e1e2
wdaoz <110090784+wdaoz@users.noreply.github.com>
2024-04-29 11:13:09 +0800

    Update index.cjs

--- index.cjs
+++ index.cjs
@@ -4,10 +4,5 @@ module.exports = Application({
   plugins: [],
   async postSave(comment) {
     // do what ever you want after comment saved
-    const reg = new RegExp('(\\d+)@qq\\.com$', 'i');
-    const mail = comment.mail;
-    if (reg.test(mail)) {
-      const q = mail.replace(/@qq\.com/i, '').toLowerCase();
-      return 'https://q1.qlogo.cn/headimg_dl?dst_uin=' + q + '&spec=4';
   },
 });
