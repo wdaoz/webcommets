c4727c3e99207f7e895a54ba27103e725bf01917
wdaoz <110090784+wdaoz@users.noreply.github.com>
2024-05-02 16:38:55 +0800

    Update index.cjs

--- index.cjs
+++ index.cjs
@@ -5,4 +5,15 @@ module.exports = Application({
   async postSave(comment) {
     // do what ever you want after comment saved
   },
+    /*
+  此方法或传入一个 comment 对象，你可以通过 comment.mail 获取邮箱。若返回值为 string 类型，则会直接调用返回值作为头像地址，否则正常生成 MD5。
+  */
+  async avatarUrl(comment) {
+      const reg = new RegExp('(\\d+)@qq\\.com$', 'i');
+      const mail = comment.mail;
+      if (reg.test(mail)) {
+        const q = mail.replace(/@qq\.com/i, '').toLowerCase();
+        return 'https://q1.qlogo.cn/headimg_dl?dst_uin=' + q + '&spec=4';
+      }
+    },
 });
